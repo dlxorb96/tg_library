eafdbd558477c330b52a7b2109a8482068372af3
dlxorb96 <xorb_1@naver.com>
2022-02-03 17:51:24 +0900

    2022.02.03

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import { createWebHashHistory, createRouter} from "vue-router";
 import Home from '@/components/Home.vue'
 import Time from '@/components/Time.vue'
 import Login from '@/components/Login.vue'
+import Lte from '@/components/Lte.vue'
 
 //URL과 컴포넌트의 연결
 const routes = [
@@ -14,6 +15,7 @@ const routes = [
     {path:'/home', name: 'Home', component: Home },
     {path: '/time', name: 'Time', component: Time},
     {path: '/login', name: 'Login', component: Login},
+    {path: '/lte', name: 'Lte', component: Lte},
 ]
 
 //실제 라우트 적용
